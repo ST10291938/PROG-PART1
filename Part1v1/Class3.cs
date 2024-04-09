a8c698396dbccd705fe10ffb17f847ddb6007666
Lesedi Molutsi <ST10291938@rcconnect.edu.za>
2024-04-09 18:51:31 +0000

    Removed dupicate methodAdded a new class, Recipe class

---
+++ Part1v1/Class3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Part1v1
+{
+     class Recipe
+    {
+        public Ingredients[] Ingredients { get; set; }
+        public string recipeName { get; set; }
+        public MyCookingMethod[] CookingMethods { get; set; }
+
+        //Constructors
+        public Recipe(string name, int ingredientCount, int stepCount) {
+        
+            recipeName = name;
+            Ingredients = new Ingredients[ingredientCount];
+            CookingMethods = new MyCookingMethod[stepCount];
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Ingredients: ");
+            foreach(var ingredient in Ingredients)
+            {
+                Console.WriteLine($"{ingredient.Quantity} {ingredient.Measurements} {ingredient.Name}");
+
+            }
+            Console.WriteLine("\nCookingMethods");
+            int methodNumber = 1;
+            foreach (var method in CookingMethods)
+            {
+                Console.WriteLine($"{methodNumber++}.{method.GetDescription()}");
+            }
+        }
+    }
+}
--- Part1v1/Program.cs
+++ Part1v1/Program.cs
@@ -6,52 +6,6 @@ namespace Part1v1
     class Program
     {
 
-        static void MyIngredients(){
-
-            Console.WriteLine("\nHow many ingredients does your recipe have? ");
-            int numOfIngredients = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\n");
-
-            String[] Ingredients = new string[numOfIngredients];
-
-            for (int i = 0; i < numOfIngredients; i++)
-            {
-                Console.WriteLine("Ingredient " + i);
-                Ingredients[i] = Console.ReadLine();
-                Console.Write("\n");
-            }
-
-            Console.WriteLine("Your ingredients are: ");
-            foreach (string ingredient in Ingredients)
-            {
-                Console.WriteLine(ingredient);
-            }
-
-        }
-
-        /* static void CookingMethod()
-         {
-             Console.WriteLine("\nHow many steps does your cooking method have? ");
-             int numOfSteps = Convert.ToInt32(Console.ReadLine());
-             Console.Write("\n");
-
-             String[] Steps = new string[numOfSteps];
-
-             for (int i = 0; i < numOfSteps; i++)
-             {
-                 Console.WriteLine("Step " + i);
-                 Steps[i] = Console.ReadLine();
-                 Console.Write("\n");
-             }
-
-             Console.WriteLine("Your ingredients are: ");
-             foreach (string step in Steps)
-             {
-                 Console.WriteLine(Steps);
-             }
-         }
-        */
-
         static void Ingredients()
         {
             Console.WriteLine("\nHow many ingredients does your recipe have? ");
@@ -86,50 +40,69 @@ namespace Part1v1
                 Console.WriteLine($"{ingredient.Quantity} {ingredient.Measurements} of {ingredient.Name}");
 
             }
-        
-
-           static void CookingMethods()
-            {
-                Console.WriteLine("How many steps does your recipe require? ");
-                int numOfSteps = Convert.ToInt32(Console.ReadLine());
-Console.Write("\n");
-
-                MyCookingMethod[] cookingMethods = new MyCookingMethod[numOfSteps];
-
-                for (int i = 0;i < numOfSteps; i++)
-                {
-                    Console.WriteLine($"Enter description of step {i + 1}: ");
-                    string description = Console.ReadLine();
-
-                    cookingMethods[i] = new MyCookingMethod(description);
-                    Console.WriteLine('\n');
-                }
-
-                Console.WriteLine("Your cooking method is as follows:");
-                foreach (MyCookingMethod cookingMethod in cookingMethods)
-                {
-                    Console.Write($"{cookingMethod.GetDescription()}\n");
-                }
-            }
 
         }
+         
          
 
 
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Green;
 
             Console.WriteLine("Hello! Welcome to your recipe application:");
             Console.WriteLine("Please enter a name for your recipe:");
             String recipeName = Console.ReadLine().ToUpper().Trim();
             Console.Write("\n" + recipeName);
 
-            Ingredients();
 
-            Console.WriteLine("Please continue to your cooking method:\n");
+
+
+            Console.WriteLine("\nHow many ingredients does your recipe require? ");
+            int numOfIngredients = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine("How many steps/methods does your recipe require? ");
+            int numOfSteps = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            Recipe recipe = new Recipe(recipeName, numOfIngredients, numOfSteps);
+         
+            for (int i = 0; i < numOfIngredients; i++)
+            {
+                Console.WriteLine($"Ingredient {i + 1}: ");
+                string name = Console.ReadLine();
+                Console.Write("\n");
+
+                Console.Write($"Enter measurements {i + 1} (e.g, cups, tablespoon, teaspoon...): \n");
+                string measurements = Console.ReadLine();
+                Console.Write("\n");
+
+
+                Console.WriteLine($"Enter quantity {i + 1} (number only): ");
+                double quantity = Convert.ToDouble(Console.ReadLine());
+
+                recipe.Ingredients[i] = new Ingredients(name, quantity, measurements);
+
+                Console.WriteLine();
+
+            }
+
+            for (int i = 0;i < numOfSteps;i++)
+            {
+                Console.WriteLine($"Enter description of step {i + 1}: ");
+                string description = Console.ReadLine();
+                Console.Write("\n");
+
+                recipe.CookingMethods[i] = new MyCookingMethod(description);
             
+        }
+            Console.WriteLine("*************************************************************************************************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine($"\nRecipe for: {recipeName}");
+            recipe.DisplayRecipe();
 
-            CookingMethods();
+
 
         }
 
@@ -145,6 +118,7 @@ Console.Write("\n");
             {
                 Console.WriteLine($"Enter description of step {i + 1}: ");
                 string description = Console.ReadLine();
+                Console.Write("\n");
 
                 cookingMethods[i] = new MyCookingMethod(description);
             }
@@ -152,7 +126,7 @@ Console.Write("\n");
             Console.WriteLine("Your cooking method is as follows:");
             foreach (MyCookingMethod cookingMethod in cookingMethods)
             {
-                Console.Write($"{cookingMethod.GetDescription()}");
+                Console.Write($"{cookingMethod.GetDescription()}\n");
             }
         }
     }
