ec68043f8f25cb81f127f1a60dc550e2bfdb1632
Lesedi Molutsi <ST10291938@rcconnect.edu.za>
2024-04-11 19:37:57 +0000

    Added comments in the Ingredients class

--- Part1v1/Class1.cs
+++ Part1v1/Class1.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Part1v1
-{
-   class Ingredients
-    {
+{// Ingredients class encapsulates the methods and details about the ingredients
+    class Ingredients
+    {//pprivate fields to store each detail abot the ingredient
         private String name;
         private String measurements;
         private double quantity;
        
-
+        //Getters and setters for the ingriendient details
         public string Name { get => name; set => name = value; }
         public string Measurements { get => measurements; set => measurements = value; }
         public double Quantity { get => quantity; set => quantity = value; }
 
-        //Constructor
+        //Constructors to intialize a new instance of an ingriendient
         public Ingredients(string name, string measurements, double quantity) {
 
             name = Name;
